236b97abf3d0eb6ae81813af96efb309bf5801b9
huyennbl <33422297+huyennbl@users.noreply.github.com>
2024-12-04 23:25:23 +0700

    test 1: getMyLocation() should return [0,0]

--- DistanceCalculator.test.js
+++ DistanceCalculator.test.js
@@ -5,6 +5,9 @@ describe('DistanceCalculator', () => {
     calculator = new DistanceCalculator()
   })
   describe('getMyLocation()', () => {
+    it('should return [0,0]', () => {
+      expect(calculator.getMyLocation()).toEqual([0, 0])
+    })
   })
   describe('getDistance()', () => {
   })
