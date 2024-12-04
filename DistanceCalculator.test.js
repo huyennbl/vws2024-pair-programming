ac4b37ef7620c7356cbb17880f393fbc6cc47720
huyennbl <33422297+huyennbl@users.noreply.github.com>
2024-12-04 23:24:25 +0700

    prepare structure

---
+++ DistanceCalculator.test.js
@@ -0,0 +1,11 @@
+import DistanceCalculator from './DistanceCalculator'
+let calculator
+describe('DistanceCalculator', () => {
+  beforeEach(() => {
+    calculator = new DistanceCalculator()
+  })
+  describe('getMyLocation()', () => {
+  })
+  describe('getDistance()', () => {
+  })
+})
